bf18c94445c1aaaa2380084a55d40a3eee335fe1
Trung <ungkientrung@gmail.com>
2019-06-09 22:51:22 +0300

    add Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import ImageList from './components/ImageList';
+import NavBar from './components/NavBar';
 
 import unsplash from './api/unsplash';
 
 export class App extends Component {
   state = { images: [] };
+
+  async componentDidMount() {
+    const res = await unsplash.get('/search/photos', {
+      params: { query: 'splash' }
+    });
+    this.setState({ images: res.data.results });
+  }
 
   onSearchSubmit = async term => {
     const res = await unsplash.get('/search/photos', {
@@ -16,10 +24,13 @@ export class App extends Component {
 
   render() {
     return (
-      <div className="App ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} name="Image Search" />
-        <ImageList images={this.state.images} />
-      </div>
+      <>
+        <NavBar />
+        <div className="App ui container" style={{ marginTop: '10px' }}>
+          <SearchBar onSubmit={this.onSearchSubmit} name="Image Search" />
+          <ImageList images={this.state.images} />
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="ui top inverted menu">
+      <div className="ui container">
+        <a className="header item" href="/">
+          <i className="large camera icon" /> Splashed
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
